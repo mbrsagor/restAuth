fa7a313348d12e093e78041c86506cc8da7bdaef
mbrsagor <mbrsagor@gmail.com>
2022-09-14 02:17:53 +0600

    New: Password change API done.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PasswordChangeAPI
 
 urlpatterns = [
-    path('change-password', PasswordChangeAPI.as_view()),
+    path('password-change/<pk>/', PasswordChangeAPI.as_view()),
 ]
