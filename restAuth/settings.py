1423e3824dfbf149ce49e452fc27f7c5102047e3
mbrsagor <mbrsagor@gmail.com>
2022-09-14 11:32:25 +0600

    New: postgres db implemented

--- restAuth/settings.py
+++ restAuth/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'restAuth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
