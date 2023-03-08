0f419f2efbfc23621662a9071bba8c6f6122a660
mbrsagor <mbrsagor@gmail.com>
2023-03-08 19:59:45 +0600

    Refactor: midleware refactor.

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,13 +1,16 @@
-from urllib import response
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
 
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
 
-def status_middleware(get_response):
-    print(f"Status: {True}")
+        print("before response")
+        response = self.get_response(request)
+        print("After response")
+        # Code to be executed for each request/response after
+        # the view is called.
 
-    def get_message(request):
-        print("Data successfully returned")
-        res = get_response(request)
-        print("Done!")
-        return res
-
-    return get_message
+        return response
